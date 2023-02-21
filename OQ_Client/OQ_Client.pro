5f1c48f1e54b3f171f120d20a68a63bbd0b48f91
Yongyu Ding <dingyongyu2002@foxmail.com>
2023-02-21 13:29:41 +0800

    add network interface

--- OQ_Client/OQ_Client.pro
+++ OQ_Client/OQ_Client.pro
@@ -16,13 +16,17 @@ DEFINES += QT_DEPRECATED_WARNINGS
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 SOURCES += \
+    loginwindow.cpp \
     main.cpp \
     mainwindow.cpp
 
 HEADERS += \
-    mainwindow.h
+    loginwindow.h \
+    mainwindow.h \
+    oqnetwork.h
 
 FORMS += \
+    loginwindow.ui \
     mainwindow.ui
 
 # Default rules for deployment.
---
+++ OQ_Client/oqnetwork.h
@@ -0,0 +1,55 @@
+#ifndef OQNETWORK_H
+#define OQNETWORK_H
+#include <QObject>
+#include <QString>
+#include <QStringView>
+#include <QVector>
+
+enum OQ_REGISTER_STATE
+{
+    OQ_REGISTER_STATE_SUCCESS,
+    OQ_REGISTER_STATE_USER_EXIST,
+    OQ_REGISTER_STATE_NETWORK_ERROR,
+    OQ_REGISTER_STATE_UNKNOWN_ERROR
+};
+
+enum OQ_LOGIN_STATE
+{
+    OQ_LOGIN_STATE_SUCCESS,
+    OQ_LOGIN_STATE_USER_ID_INVALID,
+    OQ_LOGIN_STATE_PASSWORD_INVALID,
+    OQ_LOGIN_STATE_NETWORK_ERROR,
+    OQ_LOGIN_STATE_UNKNOWN_ERROR
+};
+
+enum OQ_SEND_MESSAGE_STATE
+{
+    OQ_SEND_MESSAGE_STATE_SUCCESS,
+    OQ_SEND_MESSAGE_STATE_USER_ID_INVALID,
+    OQ_SEND_MESSAGE_STATE_EMPTY_MESSAGE,
+    OQ_SEND_MESSAGE_STATE_NETWORK_ERROR,
+    OQ_SEND_MESSAGE_STATE_UNKNOWN_ERROR
+};
+
+enum OQ_RECEIVE_MESSAGE_STATE
+{
+    OQ_RECEIVE_MESSAGE_STATE_SUCCESS,
+    OQ_RECEIVE_MESSAGE_STATE_USER_ID_INVALID,
+    OQ_RECEIVE_MESSAGE_STATE_NO_NEW_MESSAGE,
+    OQ_RECEIVE_MESSAGE_STATE_NETWORK_ERROR,
+    OQ_RECEIVE_MESSAGE_STATE_UNKNOWN_ERROR
+};
+
+class OQNetwork : public QObject
+{
+    Q_OBJECT
+public:
+    OQNetwork();
+    OQ_REGISTER_STATE registerUser(QStringView id, QStringView userName, QStringView password);
+    OQ_LOGIN_STATE login(QStringView id, QStringView password);
+    OQ_SEND_MESSAGE_STATE sendMessage(QStringView senderId, QStringView receiverId, QStringView message);
+    OQ_SEND_MESSAGE_STATE sendMessage(QStringView senderId, QStringView receiverId, const QVector<QString>& message);
+    OQ_RECEIVE_MESSAGE_STATE receiveMessage(QStringView senderId, QStringView receiverId, QVector<QString>& message);
+};
+
+#endif // OQNETWORK_H
